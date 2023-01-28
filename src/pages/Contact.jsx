24af62103c072f2c8e9a7e80b2cbd6abc5de47cb
Your Name <tuanjoker1971@gmail.com>
2023-01-28 17:29:13 +0700

    initial commit

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import "../styles/contact.css";
+
+const Contact = () => {
+  return (
+    <section>
+        <Container>
+          <Row>
+            <Col lg="7" md="7">
+              <h6 className="fw-bold mb-4">Get In Touch</h6>
+
+              <Form>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Your Name" type="text" />
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Email" type="email" />
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <textarea
+                    rows="5"
+                    placeholder="Message"
+                    className="textarea"
+                  ></textarea>
+                </FormGroup>
+
+                <button className=" contact__btn" type="submit">
+                  Send Message
+                </button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+  )
+};
+
+export default Contact;
